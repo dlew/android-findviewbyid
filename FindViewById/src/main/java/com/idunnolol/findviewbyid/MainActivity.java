544b216d2873fd1718ce6c0767796d1bf71adcfb
Daniel Lew <dan@mobiata.com>
2013-11-16 09:17:18 -0800

    Setup basic test Activity structure

--- FindViewById/src/main/java/com/idunnolol/findviewbyid/MainActivity.java
+++ FindViewById/src/main/java/com/idunnolol/findviewbyid/MainActivity.java
@@ -2,13 +2,45 @@ package com.idunnolol.findviewbyid;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends ActionBarActivity {
+
+    private ViewGroup mContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContainer = (ViewGroup) findViewById(R.id.container);
+
+        findViewById(R.id.add_depth_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        findViewById(R.id.add_child_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        findViewById(R.id.run_test_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        findViewById(R.id.reset_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
 }
